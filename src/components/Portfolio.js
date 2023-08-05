1358e2e2e6260ba4c51a34c2ef632bce9ab52317
Sergio Sorrosal <sergiosorrosalgayan@gmail.com>
2023-08-05 10:52:10 +0200

    Maquetación portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { trabajos } from '../data/trabajos';
+import { Link } from 'react-router-dom';
 
 export function Portfolio() {
     return (
         <div className='page'>
             <h1 className='heading'>Portfolio</h1>
-        </div>
+            <section className='works'>
+                {
+                    trabajos.map(trabajo => {
+                        return (
+                            <article key={trabajo.id} className='work-item'>
+                                <div className='mask'>
+                                    <img src={"/images/" + trabajo.id + ".png"} />
+                                </div>
+                                <span>{trabajo.categorias}</span>
+                                <h2><Link to={"/proyecto/" + trabajo.id}>{trabajo.nombre}</Link></h2>
+                                <h3>{trabajo.tecnologias}</h3>
+                            </article>
+                        )
+                    })
+                }
+            </section>
+        </div >
     )
 }
